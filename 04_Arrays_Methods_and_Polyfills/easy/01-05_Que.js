326f71c132c7a4ea4f57205810b957403ce0e9a1
anujGarola26 <garolaanuj1@gmail.com>
2026-09-07 00:45:40 +0530

    Q1 solution 2  module 4 easy

--- 04_Arrays_Methods_and_Polyfills/easy/01-05_Que.js
+++ 04_Arrays_Methods_and_Polyfills/easy/01-05_Que.js
@@ -20,3 +20,26 @@ function findMinMax(arr){
 }
 
 findMinMax([10, 4, -99, 1000, 43, 5, 8, 3, 7, 21, 6, -8]);
+
+
+// Approach 2
+function findMinMax2(arr){
+  let min= arr[0];
+  let max = arr[0];
+  for(let i=0; i <= arr.length; i++){
+    let val = arr[i]
+
+    if( val > max){
+      max = val
+    }else if(val < min){
+      min = val
+    }
+    }
+  
+    // if(arr[i] < arr[i + 1]){
+    //   min = arr[i];
+    // }
+  return {min, max};
+}
+
+console.log(findMinMax2([10, 4, -99, 1000, 43, 5, 8, 3, 7, 21, 6, -8]));
