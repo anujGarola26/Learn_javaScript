4f1d0366479cc6bbe16f323aaf44851726ba1304
anujGarola26 <garolaanuj1@gmail.com>
2026-09-06 11:17:53 +0530

    practice Q2/Soln 2 easy..

--- 01_JS_Fundamentals_and_Memory/easy/01-05_Que.js
+++ 01_JS_Fundamentals_and_Memory/easy/01-05_Que.js
@@ -10,3 +10,30 @@ function shallowClone(arr) {
 }
 
 console.log(shallowClone([2, 3, 4, "anuj", {}]));
+
+// Shallow clones for every test cases
+
+function shallowClone1(source) {
+  if (typeof source !== "object" || source === null) {
+    return "Primitive or Null datatypes";
+  }
+  let isArr = Array.isArray(source);
+  let newArr = [];
+  if (isArr) {
+    for (let i = 0; i < source.length; i++) {
+      newArr.push(source[i]);
+    }
+    return newArr;
+  }
+  let obj = {};
+  for (key in source) {
+    if (Object.prototype.hasOwnProperty.call(source, key)) {
+      obj[key] = source[key];
+    }
+  }
+  return obj;
+}
+console.log(shallowClone1({ name: "Aman", scores: [90, 95] }));
+console.log(shallowClone1(95));
+console.log(shallowClone1(undefined));
+console.log(shallowClone1(null));
