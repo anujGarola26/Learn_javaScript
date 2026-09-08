72e336d51f62d9fdf2221bd6d3083755e4bf812d
anujGarola26 <garolaanuj1@gmail.com>
2026-09-08 12:59:21 +0530

    Q2 solution module 6 easy

--- 06_Strings_Regex_and_Manipulations/easy/01-05_Que.js
+++ 06_Strings_Regex_and_Manipulations/easy/01-05_Que.js
@@ -21,3 +21,32 @@ console.log(reverseString('adam'));
 console.log(reverseString([]));
 console.log(reverseString({}));
 console.log(reverseString(null));
+
+
+// Problem 2: Valid Palindrome Verification (Ignoring Non-Alphanumeric Characters)
+
+
+function isPalindrome(str1){
+  if(typeof str1 !== "string"){
+    return "Not a string"
+  }
+  if(str1.length <= 1){
+    return "Too small string"
+  }
+  let str = str1.replaceAll(" ", "").toLowerCase();
+  let newStr = str.split('').reverse().join("");
+  if(str !== newStr){
+    return false
+  }
+
+  return true
+}
+
+console.log(isPalindrome('anuj'));
+console.log(isPalindrome({}));
+console.log(isPalindrome(['anuj']));
+console.log(isPalindrome(null));
+console.log(isPalindrome(2324));
+console.log(isPalindrome('laal'));
+console.log(isPalindrome('race a car'));
+console.log(isPalindrome('A man, a plan, a canal'));
